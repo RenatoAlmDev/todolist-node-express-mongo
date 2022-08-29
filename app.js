7af552d2086169565f3baf8f8b1d5aa60394552d
Renato Almeida <almeida.renato1988@gmail.com>
2022-08-29 15:29:31 -0300

    Estruturação da aplicação com métodos Post, Put e Delete, instalação de Method Override

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ const path = require("path");
 
 const checkListRouter = require("./src/routes/checklist");
 const rootRouter = require("./src/routes/index");
+const methodOverride = require("method-override");
 
 require("./config/database");
 
 const app = express();
 app.use(express.json());
+//middleware para quando chega requisição via formulário:
+app.use(express.urlencoded({ extended: true }));
+// passaremos mais um parâmetro pro override, porque ele atua diretamente sobre o POST, com o novo parâmetro ele vai atuar sobre o GET também:
+app.use(methodOverride("_method", { methods: ["POST", "GET"] }));
 
 // habilitando o projeto para usar arquvios estáticos:
 app.use(express.static(path.join(__dirname, "public")));
